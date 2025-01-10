e9e2ac49a705910a88d5dbc0d91acc2c630a5854
Ariel Hurtado <arielhurtado388@gmail.com>
2025-01-09 23:15:18 -0500

    Dotenv y conexion a la BD lista

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conexion a la BD
+try {
+  await db.authenticate();
+  console.log("Conexion correcta");
+} catch (error) {
+  console.log(error);
+}
 
 // Habilitar pug
 app.set("view engine", "pug");
