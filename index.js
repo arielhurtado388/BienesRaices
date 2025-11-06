63c4d2337c2f61feda20e74152d734ea04e5903e
Ariel Hurtado <arielhurtado388@gmail.com>
2025-11-06 14:46:25 -0500

    ORM y .env

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conexion a la DB
+try {
+  await db.authenticate();
+  console.log("Conexion correcta a la DB");
+} catch (error) {
+  console.log(error);
+}
 
 // Habilitar Pug
 app.set("view engine", "pug");
